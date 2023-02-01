3a39bfcbb286be2e37faec9a0b942193cc1a2509
SHARUN266 <sharunattari266@gmail.com>
2023-02-01 15:35:31 +0530

    GetImage

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,21 @@ function App() {
   const {url}=useSelector((state)=>state.home)
   const dispatch=useDispatch()
  useEffect(()=>{
-   
-   fetchDataFromApi("/movie/popular").then((res)=>dispatch(getApiConfiguratoin(res)))
+  fetchApiConfig()
+  
  },[])
+
+ const fetchApiConfig=()=>{
+  fetchDataFromApi("/configuration").then((res)=>{
+    const url={
+      backdrop:res.images.secure_base_url+"original",
+      poster:res.images.secure_base_url+"original",
+      profile:res.images.secure_base_url+"original"
+    }
+    
+    dispatch(getApiConfiguratoin(url))})
+
+ }
 
   return (
     <>
--- src/pages/Home/HeroBanner/HeroBanner.jsx
+++ src/pages/Home/HeroBanner/HeroBanner.jsx
@@ -1,18 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
 
 export default function HeroBanner() {
-    const nav=useNavigate()
+    const nav=useNavigate();
+    const {url}=useSelector((state)=>state.home)
+    const {error,loading,data}=useFetch("/movie/upcoming")
     const [backGround,setBackGround]=useState("")
     const [query,setQuery]=useState("");
+    useEffect(()=>{
+       
+        const bg=url.backdrop+data?.results[Math.floor(Math.random()*20)]?.backdrop_path;
+        setBackGround(bg)
+    },[data])
     const searchQueryHandler=(event)=>{
+    
         if(event.key=="Enter"&& query.length>0){
             nav(`/search/${query}`)
         }
 
     }
+   
   return (
     <div className="heroBanner">
+        <div className="backdrop-img">
+            <img src={backGround} alt="" />
+        </div>
       <div className="wrapper">
         <div className="heroBanner">
           <span className="title">Welcome</span>
