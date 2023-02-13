43b172bd47e04f226b93c058fb8cbacd4bdcf8ff
SHARUN266 <sharunattari266@gmail.com>
2023-02-13 22:07:25 +0530

    Trending

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroBanner from './HeroBanner/HeroBanner'
 import "./Home.scss"
+import Trending from './Trending/Trending'
 export default function Home() {
   return (
     <div className='homePage'>
         <HeroBanner/>
-        <div style={{height:"500vh"}}></div>
+        <Trending/>
+        
     </div>
   )
 }
