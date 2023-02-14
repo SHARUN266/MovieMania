a7dace013e2b83965009b24a252709470f1a6704
SHARUN266 <sharunattari266@gmail.com>
2023-02-14 09:13:49 +0530

    carousel&switchAdd

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import HeroBanner from './HeroBanner/HeroBanner'
-import "./Home.scss"
-import Trending from './Trending/Trending'
+import React from "react";
+import HeroBanner from "./HeroBanner/HeroBanner";
+import "./Home.scss";
+import Trending from "./Trending/Trending";
 export default function Home() {
   return (
-    <div className='homePage'>
-        <HeroBanner/>
-        <Trending/>
-        
+    <div className="homePage">
+      <HeroBanner />
+      <Trending />
     </div>
-  )
+  );
 }
--- src/pages/Home/Trending/Trending.jsx
+++ src/pages/Home/Trending/Trending.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import Carousel from "../../../components/Carousel/Carousel";
+import ContentWrapper from "../../../components/ContentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/SwitchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
 
 export default function Trending() {
+  const [endpoint,setEndpoint]=useState("day")
+  const {data,loading}=useFetch(`/trending/all/${endpoint}`)
+  const onTabChange = (tab) => {
+    setEndpoint(tab==="Day"?"day":"week")
+  };
+ 
   return (
-    <div>Trending</div>
-  )
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel  data={data?.results} loading={loading}  />
+    </div>
+  );
 }
